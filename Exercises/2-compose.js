9d0bbc96a05f8ee380aa635d592054ab369443a2
edw4rdkk <eduardkyrychenko923@gmail.com>
2024-12-09 00:41:17 +0200

    first commit

--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,26 @@
-'use strict';
+"use strict";
 
-const compose = (...fns) => (x) => null;
+const compose = (...fns) => {
+  const handlers = [];
+  const inner = (x) => {
+    let res = x;
+    const last = fns.length - 1;
+    try {
+      for (let i = last; i >= 0; i--) {
+        res = fns[i](res);
+      }
+      return res;
+    } catch (error) {
+      for (const handler of handlers) {
+        handler(error);
+      }
+      return undefined;
+    }
+  };
+  inner.on = (name, handler) => {
+    if (name === "error") handlers.push(handler);
+  };
+  return inner;
+};
 
 module.exports = { compose };
